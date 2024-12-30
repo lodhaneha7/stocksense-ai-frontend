e285fddd19dc549c55cb1e4ef3b07bca97776f6b
lodhaneha7 <nhlodha7@gmail.com>
2024-12-30 22:00:49 +0530

    update env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   production: false,
   baseUrl: 'http://localhost:3000', 
   indianStockAPiEndpoint: 'https://stock.indianapi.in',
-  indianStockAPiKey: process.env.INDIAN_STOCK_API_KEY, // loaded from .env file
+  indianStockAPiKey: 'your-api-key-here', 
 };
